62439e5c8f174752cd5774c3f240b7cc8fa158ad
Jack Minardi <jack@minardi.org>
2014-01-30 20:00:52 -0500

    add packaging boilerplate

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+INFO = {'name': 'mecode',
+        'version': '0.0.1',
+        }
+
+setup(
+    name=INFO['name'],
+    version=INFO['version'],
+    author='Jack Minardi',
+    packages=find_packages(),
+    zip_safe=False,
+    maintainer='Jack Minardi',
+    maintainer_email='jack@minardi.org',
+)
