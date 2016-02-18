f4a6c8d76c50326b0d1f4935e27ed5d319c552c8
Jack Minardi <jack@minardi.org>
2016-02-17 20:14:25 -0500

    rearange setup.py to get ready for pypi

--- setup.py
+++ setup.py
@@ -2,18 +2,22 @@ from setuptools import setup, find_packages
 
 INFO = {'name': 'mecode',
         'version': '0.2.2',
+        'author': 'Jack Minardi',
+        'author_email': 'jack@minardi.org',
         }
 
 setup(
+    name=INFO['name'],
+    version=INFO['version'],
+    author=INFO['author'],
+    author_email=INFO['author_email'],
+    packages=find_packages(),
+    url='https://github.com/jminardi/mecode',
+    download_url='https://github.com/jminardi/mecode/tarball/master',
+    keywords=['gcode', '3dprinting', 'cnc', 'reprap'],
+    zip_safe=False,
     data_files = [
         ("./mecode", [
             "mecode/header.txt",
             "mecode/footer.txt"])],
-    name=INFO['name'],
-    version=INFO['version'],
-    author='Jack Minardi',
-    packages=find_packages(),
-    zip_safe=False,
-    maintainer='Jack Minardi',
-    maintainer_email='jack@minardi.org',
 )
