c020ac386ac028e2f43bcfa498c251ca54a86a93
Debian Live user <user@localhost.localdomain>
2016-10-20 18:37:10 +0000

    Add return statements and complete methods

--- mecode/devices/efd_pico_pulse.py
+++ mecode/devices/efd_pico_pulse.py
@@ -31,39 +31,41 @@ class EFDPicoPulse(object):
     def send(self, command):
         msg = command + EOT
         self.s.write(msg)
-        self.s.read_until(ACK)
+        # return response and remove ACK
+        return self.s.read_until(ACK)[:-2]
 
-    def set_valve_mode(mode):
-
-
+    def set_valve_mode(self, mode):
+        return self.send(str(mode) + 'drv1')
 
     def set_dispense_count(self, count):
-
-
+        return self.send('{:05}'.format(count) + 'dcn1')
 
     def get_valve_status(self):
-        self.send('rdr1')
+        return self.send('rdr1')
 
     def cycle_valve(self):
-        self.send('1cycl')
-        self.send('0cycl')
+        return self.send('1cycl') + self.send('0cycl')
 
     def set_heater_mode(self, mode):
+        """Set heater mode to off, on, or return status.
 
-
+        Keyword argument:
+        mode -- 0 = off; 1 = on; 2 = status; 3 = remote mode"""
+        return self.send(str(mode) + 'chtr')
 
     def set_heater_temp(self, temp):
-        
+        """Set heater target to temp between 0-100C."""
+        return  self.send('{:05.1f}'.format(temp) + 'stmp')
 
     def get_heater_status(self):
-        self.send('rhtr')
+        return self.send('rhtr')
 
     def get_valve_info(self):
-        self.send('info')
+        return self.send('info')
 
     def get_alarm_hist(self):
-        self.send('ralr')
+        return self.send('ralr')
 
     def reset_alarm(self):
-        self.send('arst')
+        return self.send('arst')
     
