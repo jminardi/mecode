df6ebf3c3c55cb795a2fa68cbdef3a08ac6372d7
Jack Minardi <jack@minardi.org>
2016-11-16 21:52:38 -0500

    Bump version number to 0.2.8

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 INFO = {'name': 'mecode',
-        'version': '0.2.7',
+        'version': '0.2.8',
         'description': 'Simple GCode generator',
         'author': 'Jack Minardi',
         'author_email': 'jack@minardi.org',
@@ -16,7 +16,7 @@ setup(
     packages=find_packages(),
     url='https://github.com/jminardi/mecode',
     download_url='https://github.com/jminardi/mecode/tarball/master',
-    keywords=['gcode', '3dprinting', 'cnc', 'reprap'],
+    keywords=['gcode', '3dprinting', 'cnc', 'reprap', 'additive'],
     zip_safe=False,
     package_data = {
         '': ['*.txt', '*.md'],
