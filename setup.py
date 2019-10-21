20870de526e7c855a490fd86a31b756d8a5c69b4
Jack Minardi <jack@minardi.org>
2019-10-20 23:32:11 -0400

    Bump version to 0.3.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 INFO = {'name': 'mecode',
-        'version': '0.2.9',
+        'version': '0.3.0',
         'description': 'Simple GCode generator',
         'author': 'Jack Minardi',
         'author_email': 'jack@minardi.org',
