a6c519545b17c0faf88a4e73d7624c940931bf8c
Rodrigo Telles <rtellez700@gmail.com>
2023-03-20 22:20:39 -0700

    fix typo

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from os import path
 from setuptools import setup, find_packages
 
 INFO = {'name': 'mecode',
-        'version': '0.4.4',
+        'version': '0.4.5',
         'description': 'Simple GCode generator',
         'author': 'Rodrigo Telles',
         'author_email': 'rtelles@g.harvard.edu',
