1c36d38177a2e55db77712a1544078b796159717
Rodrigo Telles <rtellez700@gmail.com>
2023-03-20 20:56:40 -0700

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from os import path
 from setuptools import setup, find_packages
 
 INFO = {'name': 'mecode',
-        'version': '0.4.2',
+        'version': '0.4.3',
         'description': 'Simple GCode generator',
         'author': 'Rodrigo Telles',
         'author_email': 'rtelles@g.harvard.edu',
